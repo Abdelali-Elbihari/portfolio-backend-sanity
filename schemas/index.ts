2be6e0271f828234ae46afaea6e2ca63a33a0bf4
Elbihari, Abdelali [Global Data & Content] <Abdelali.Elbihari@solera.com>
2023-12-13 09:24:07 +0100

    Add Backend Sanity

---
+++ schemas/index.ts
@@ -0,0 +1,8 @@
+import abouts from "./abouts";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import workExperience from "./workExperience";
+import projects from "./projects";
+
+export const schemaTypes = [projects, abouts, skills, workExperience, experiences, contact]
